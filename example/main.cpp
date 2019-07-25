5b05a1e53532bcc6f941ab7ec72cfa0e3c197cf6
Theo Bessel <43089275+TheoBessel@users.noreply.github.com>
2019-07-25 12:36:14 +0200

    Commit Matrix class
    TODO : Add mathematical operations on Matrix

---
+++ example/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "maths/Matrix.hpp"
+
+int main() {
+    Matrix mat({
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {3, 2, 4, 5},
+            {3, 2, 1, 4}
+        }
+    );
+
+    for (int j = 0; j < mat.height(); ++j) {
+        for (int i = 0; i < mat.width(); ++i) {
+            std::cout << static_cast<int>(mat(i, j)) << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
---
+++ includes/maths/Matrix.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Theo Bessel on 2019-07-24.
+//
+
+#ifndef INTELLIRECO_MATRIX_HPP
+#define INTELLIRECO_MATRIX_HPP
+
+#include <vector>
+
+template < typename T = uint8_t >
+class Matrix {
+public:
+    Matrix(std::initializer_list<std::initializer_list< T > >);
+    T operator()(uint8_t, uint8_t);
+    std::size_t size();
+    std::size_t width();
+    std::size_t height();
+private:
+    std::size_t m_row;
+    std::size_t m_column;
+    std::vector< T > m_mat;
+};
+
+#include "Matrix.inl"
+
+#endif //INTELLIRECO_MATRIX_HPP
